f5dd24ca52ae48c6fbe8615dcb3138e0a8c4d9e8
b1g.dr.0p <b1g.dr.0p@protonmail.com>
2019-07-20 19:53:36 +0000

    Simple TCP client in python 3

---
+++ python3/README.md
@@ -0,0 +1,2 @@
+# python3
+This code is written for python3 (Python 3.7) instead of python2 which is used in the book.
---
+++ python3/chapter02/tcp_client.py
@@ -0,0 +1,25 @@
+"""
+Simple TCP client
+
+See https://docs.python.org/3/library/socket.html for
+more information about the socket library
+"""
+import socket
+
+target_host = 'www.google.com'
+target_port = 80
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((target_host, target_port))
+
+# send some data
+client.send(b'GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n')
+
+# receive some data
+response = client.recv(4096)
+
+print(response)
+
